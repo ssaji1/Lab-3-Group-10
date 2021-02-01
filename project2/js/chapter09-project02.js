edbc78b6c3d09a0323ecce471304b0e247b0764e
mjohny <65822793+MereenaJohny@users.noreply.github.com>
2021-02-01 18:00:30 -0500

    step 3

--- project2/js/chapter09-project02.js
+++ project2/js/chapter09-project02.js
@@ -1,4 +1,16 @@
 /* add code here */
 window.addEventListener('load', function(){// creating event handler after loading window
-
+    var imageClick = document.querySelectorAll('#thumbnails img'); 
+    var i = 0;
+    while (i < imageClick.length) {
+        imageClick[i].addEventListener("click", function () {
+            var splited = event.target.src.split("images/small/");
+            document.querySelector("#featured>img").src = "images/medium/" + splited[1];
+            document.querySelector("#featured>img").title = event.target.title;
+            document.querySelector("#featured>img").alt = event.target.alt;
+            var figuretitle = document.querySelector("#featured > figcaption")
+            figuretitle.innerHTML = event.target.title;
+        })
+        i++;
+    }
 })
